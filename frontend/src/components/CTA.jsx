5ca05e8f5b212071901c8c68d85203e823195155
test <test>
2025-05-29 09:46:52 -0700

    update

--- frontend/src/components/CTA.jsx
+++ frontend/src/components/CTA.jsx
@@ -139,7 +139,7 @@ const CTA = () => {
             </nav>
             <aside>
                 <p>Designed and Developed by Jose Cruz.
-                Built with <span className='text-teal-400'>Coffee</span> & <span className='text-teal-400'>Love</span>.
+                Built with <span className='text-teal-400'>Coffee</span> & <span className='text-teal-400'>Love</span>.</p>
             </aside>
         </footer>
     )
