700b1ccb674fb5851a542f8b14f22eb27e4af7ef
test <test>
2025-06-01 10:31:28 -0700

    update

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}) => {
     return (
-        <div className="container mx-auto px-8 sm:px-10 lg:px-12 bg-base-100">
+        <div className="container mx-auto px-8 sm:px-10 lg:px-12 bg-base-100 overflow-x-hidden">
             {children}
         </div>
     )
