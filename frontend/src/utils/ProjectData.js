c9b7f28a409b8382a170b76cf8ddc1d80640acf0
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-10-04 14:03:04 -0700

    update

--- frontend/src/utils/ProjectData.js
+++ frontend/src/utils/ProjectData.js
@@ -1,14 +1,15 @@
 import vitalwear from "../assets/vitalwear.png"
 import Xclone from "../assets/Xclone.png"
+import reactABCFitness from "../assets/reactABCFitness.png"
 
 const ProjectData = [
     {
         id: 1,
-        title: "VitalWear",
+        title: "ABC Fitness",
         description: "When I started learning web-development, it has always been my goal to build a e-commerce site as one of my personal projects. Since I am passionate about fitness, I decided to build a fitness related e-commerce platform. This site consisted of react-redux for state management, allowing users to add and remove items from their cart, this site is also fully responsive as well. Since this was my very first project I do intend on revisiting it and adding more features to this site. The tech stack that was used to build this site is react, react-redux, and bootstrap.",
-        image: vitalwear, 
-        sourceCode: "https://github.com/Cruz3196/vitalwear",
-        liveDemo: "https://vitalwear.netlify.app"
+        image: reactABCFitness, 
+        sourceCode: "https://github.com/Cruz3196/abcfitness-react/tree/main",
+        liveDemo: "https://abcfitness-react.onrender.com/"
     },
     {
     id: 2,
@@ -17,7 +18,15 @@ const ProjectData = [
     image: Xclone, 
     sourceCode: "https://github.com/Cruz3196/X-Clone",
     liveDemo: "https://x-clone-fz67.onrender.com/login"
-    }
+    },
+    {
+    id: 1,
+    title: "VitalWear",
+    description: "When I started learning web-development, it has always been my goal to build a e-commerce site as one of my personal projects. Since I am passionate about fitness, I decided to build a fitness related e-commerce platform. This site consisted of react-redux for state management, allowing users to add and remove items from their cart, this site is also fully responsive as well. Since this was my very first project I do intend on revisiting it and adding more features to this site. The tech stack that was used to build this site is react, react-redux, and bootstrap.",
+    image: vitalwear, 
+    sourceCode: "https://github.com/Cruz3196/vitalwear",
+    liveDemo: "https://vitalwear.netlify.app"
+    },
 ]
 
 export default ProjectData;
