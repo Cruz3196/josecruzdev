9b20ae40416450cdc6811a9e4904995c3a2846e7
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-10-04 14:36:41 -0700

    new project added  to my portfolio

--- frontend/src/utils/ProjectData.js
+++ frontend/src/utils/ProjectData.js
@@ -1,12 +1,12 @@
 import vitalwear from "../assets/vitalwear.png"
 import Xclone from "../assets/Xclone.png"
-import reactABCFitness from "../assets/reactABCFitness.png"
+import reactABCFitness from "../assets/ReactABCFitness.png"
 
 const ProjectData = [
     {
         id: 1,
         title: "ABC Fitness",
-        description: "When I started learning web-development, it has always been my goal to build a e-commerce site as one of my personal projects. Since I am passionate about fitness, I decided to build a fitness related e-commerce platform. This site consisted of react-redux for state management, allowing users to add and remove items from their cart, this site is also fully responsive as well. Since this was my very first project I do intend on revisiting it and adding more features to this site. The tech stack that was used to build this site is react, react-redux, and bootstrap.",
+        description: "For this project, I wanted to revisit a old project that I built for a web development course I took on Sophia Learning. The original project was built using only HTML, CSS, and JavaScript, the user at the time was able to add and delete products using Local and Session Storage. For this updated version of the site, I got inspiration from a Udemy Course, I was taking for full stack development. I also research other repos on Github to get more ideas on how to structure the backend and add more features to the site. After completing each route and controller, I then tested the endpoints using Postman. The techstack that was used to build this site is React, Node.js, Express.js, MongoDB, and Mongoose, and dev dependencies that were used for the backend are axios, bcryptjs, cors, jsonwebtoken, cookie-parser, crypto, express and much more. The frontend dependencies that were used are axios, react-router-dom, zustard, axios, tailwindcss with daisyUI, and react-icons.",
         image: reactABCFitness, 
         sourceCode: "https://github.com/Cruz3196/abcfitness-react/tree/main",
         liveDemo: "https://abcfitness-react.onrender.com/"
