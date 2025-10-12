03b2aba86749d3f2a22699e471c47e76b1965ce5
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2025-10-12 16:38:34 -0700

    read me file has been update and corrected the duplicate ID in the projectData.js file

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# josecruzportfolio
+# Jose Cruz Portfolio
+
+## Overview
+I wanted to rebuild my portfolio site. and since my first attempt was a HTML template, I just didn't feel like I actually did anything. For this project since I have been learning ReactJs, Tailwind, and Express Js; I decided to just build a small full stack application that consist of node mailer with Gmail as a service. 
+
+## Features
+This project is responsive and has dark and light mode. Also, the email functionality does work in which I used Gmail's service. 
+
+## Running the project 
+In order to run this project, you would first need to git clone. Once the project has been cloned in the main directory run <npm run build> this will generate "dist" folder in the frontend. 
+
+### Disable the content security policy (CSP)
+In both the dist/index.html and frontend/index.html files, comment out the following line: 
+
+``` bash <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> ```
+
+
+ Once that is done you can now run npm run dev in the main directory and this will run the server on 5001, then on a separate terminal cd to the frontend directory and run npm run dev, this will run the front end react on port 5173. 
+
+## Email Setup
+
+This project uses email functionality. To enable it:
+
+1. Get your email credentials:
+   - **Gmail**: Enable 2FA and create an [App Password](https://support.google.com/accounts/answer/185833)
+   - **SendGrid**: Sign up and get an API key
+   - **Other**: Use your SMTP credentials
+
+2. Add to your `.env` file:
+```bash
+EMAIL_USER=your-email@gmail.com
+EMAIL_PASS=your-app-password
+EMAIL_SERVICE=gmail 
+```
+
+## Dependencies 
+###### Frontend 
+1. TailwindCss
+2. Bootstrap-Icons
+3. DaisyUi
+4. react 
+5. react-dom
+6. react-router-dom
+7. theme-change 
+
+###### Backend
+1. Cors
+2. Cross-env
+3. express
+4. nodemailer
--- frontend/src/utils/ProjectData.js
+++ frontend/src/utils/ProjectData.js
@@ -4,12 +4,12 @@ import reactABCFitness from "../assets/ReactABCFitness.png"
 
 const ProjectData = [
     {
-        id: 1,
-        title: "ABC Fitness",
-        description: "For this project, I wanted to revisit a old project that I built for a web development course I took on Sophia Learning. The original project was built using only HTML, CSS, and JavaScript, the user at the time was able to add and delete products using Local and Session Storage. For this updated version of the site, I got inspiration from a Udemy Course, I was taking for full stack development. I also research other repos on Github to get more ideas on how to structure the backend and add more features to the site. After completing each route and controller, I then tested the endpoints using Postman. The authentication controller is also located within the user routes. It uses bcrypt, JSON Web Tokens (JWT), and cookie-parser. When a user creates an account, a token is generated to authenticate the user during login. This includes both access and refresh tokens. The refresh token resets every 15 minutes using a Redis cache system. The access token is used to log in to the account, and passwords are hashed using bcrypt. Middleware checks are implemented to determine if the user is an admin or has a trainer profile. The techstack that was used to build this site is React, Node.js, Express.js, MongoDB, and Mongoose, and dev dependencies that were used for the backend are axios, bcryptjs, cors, jsonwebtoken, cookie-parser, crypto, express and much more. The frontend dependencies that were used are axios, react-router-dom, zustard, axios, tailwindcss with daisyUI, and react-icons.",
-        image: reactABCFitness, 
-        sourceCode: "https://github.com/Cruz3196/abcfitness-react/tree/main",
-        liveDemo: "https://abcfitness-react.onrender.com/"
+    id: 1,
+    title: "ABC Fitness",
+    description: "For this project, I wanted to revisit a old project that I built for a web development course I took on Sophia Learning. The original project was built using only HTML, CSS, and JavaScript, the user at the time was able to add and delete products using Local and Session Storage. For this updated version of the site, I got inspiration from a Udemy Course, I was taking for full stack development. I also research other repos on Github to get more ideas on how to structure the backend and add more features to the site. After completing each route and controller, I then tested the endpoints using Postman. The authentication controller is also located within the user routes. It uses bcrypt, JSON Web Tokens (JWT), and cookie-parser. When a user creates an account, a token is generated to authenticate the user during login. This includes both access and refresh tokens. The refresh token resets every 15 minutes using a Redis cache system. The access token is used to log in to the account, and passwords are hashed using bcrypt. Middleware checks are implemented to determine if the user is an admin or has a trainer profile. The techstack that was used to build this site is React, Node.js, Express.js, MongoDB, and Mongoose, and dev dependencies that were used for the backend are axios, bcryptjs, cors, jsonwebtoken, cookie-parser, crypto, express and much more. The frontend dependencies that were used are axios, react-router-dom, zustard, axios, tailwindcss with daisyUI, and react-icons.",
+    image: reactABCFitness, 
+    sourceCode: "https://github.com/Cruz3196/abcfitness-react/tree/main",
+    liveDemo: "https://abcfitness-react.onrender.com/"
     },
     {
     id: 2,
@@ -20,7 +20,7 @@ const ProjectData = [
     liveDemo: "https://x-clone-fz67.onrender.com/login"
     },
     {
-    id: 1,
+    id: 3,
     title: "VitalWear",
     description: "When I started learning web-development, it has always been my goal to build a e-commerce site as one of my personal projects. Since I am passionate about fitness, I decided to build a fitness related e-commerce platform. This site consisted of react-redux for state management, allowing users to add and remove items from their cart, this site is also fully responsive as well. Since this was my very first project I do intend on revisiting it and adding more features to this site. The tech stack that was used to build this site is react, react-redux, and bootstrap.",
     image: vitalwear, 
